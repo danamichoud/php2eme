9a94467d456500756d1c45ff0663b5f39baca266
Dana <dana.michoud@cpnv.ch>
2015-06-16 15:40:26 +0200

    L'exercice chapitre 4

---
+++ chap3ex1.php
@@ -0,0 +1,27 @@
+<?php
+/* ---------------------------------------------------------------------
+ *                        Exercice 1                           
+ * Nom du fichier: ajout_utilisateur.php
+ * Auteur: PKR 
+ * Date: Janvier 2008
+ * Description: Ce script affiche un formulaire d'inscription                              
+ * ---------------------------------------------------------------------
+ */
+ $fondpropre =_Post
+// Vous avez le fond propre nécessaire
+//  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+
+  <h2>Calcul d'intérêts</h2>
+    <php
+  if ($fp
+
+</body>
+</html>
